e8c5df69dc01ee5e9a5c26ea323516de296ec13c
Abdlrhman <abdlrhmanv@icloud.com>
2024-12-13 07:57:08 +0200

    Added html , css and js files

---
+++ quiz.js
@@ -0,0 +1,17 @@
+functionc checkAnswer() {
+    const correctAnswer = "4";
+
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = selectedOption ? selectedOption.value : null;
+
+    const feedbackElement + document.getElementById("feedback");
+
+    if(userAnswer==correctAnswer) {
+        feedbackElement.textContent ="Correct! Well done.";
+    }
+    else {
+        feedbackElement.textContent = "That's incorrect. Try again!";
+    }
+}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer)
